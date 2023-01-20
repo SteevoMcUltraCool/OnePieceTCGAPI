efb8492aef19452e6b7a79d1bd4ba6e97d8a2c5a
SteevoMcUltraCool <113132373+SteevoMcUltraCool@users.noreply.github.com>
2023-01-20 10:45:04 -0500

    Create cards.js

---
+++ cards.js
@@ -0,0 +1,160 @@
+{
+  "ref": Ref(Collection("Cards"), "354307479772856407"),
+  "ts": 1674227295990000,
+  "data": {
+    "0000": {
+      "key": "0000",
+      "displayName": "Crocodile",
+      "type": "Leader",
+      "colors": [
+        "blue"
+      ],
+      "subtypes": [
+        "The Seven Warlords of the Sea",
+        "Baroque Works"
+      ],
+      "damageType": "Special",
+      "image": {
+        "regular": "https://product-images.tcgplayer.com/fit-in/437x437/288266.jpg",
+        "special": ""
+      },
+      "box": "ST03",
+      "count": 1,
+      "boxPosition": "001",
+      "cardID": "ST03-001",
+      "artwork": {
+        "half": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fonepiece.fandom.com%2Fwiki%2FCrocodile&psig=AOvVaw3AIn-HYZbajy8TD_b9UUsD&ust=1674240672202000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCMiOg9em1PwCFQAAAAAdAAAAABAQ",
+        "full": "https://static.wikia.nocookie.net/villains/images/0/08/Crocodile_OP_render.png/revision/latest?cb=20210815013824"
+      },
+      "health": 5,
+      "power": 5000,
+      "cost": "N/A",
+      "counter": "N/A",
+      "text": "[Activate: Main] [Once Per Turn] DON!! -4 (You may return the specified number of DON!! cards from your field to your DON!! deck.): Return up to 1 Character with a cost of 5 or less to the owner's hand.",
+      "textAsHTML": "<span class=\"blue curved\">Activate: Main</span> <span class=\"red round\">Once Per Turn<span> <b>DON!! -4</b> <i>(You may return the specified number of DON!! cards from your field to your DON!! deck.)</i>: Return up to 1 Character with a cost of 5 or less to the owner's hand."
+    },
+    "0001": {
+      "key": "0001",
+      "displayName": "Crocodile",
+      "type": "Character",
+      "colors": [
+        "blue"
+      ],
+      "damageType": "Special",
+      "subtypes": [
+        "The Seven Warlords of the Sea",
+        "Baroque Works"
+      ],
+      "image": {
+        "regular": "https://product-images.tcgplayer.com/fit-in/656x656/453519.jpg",
+        "special": ""
+      },
+      "box": "OPO1",
+      "count": "N/A",
+      "boxPosition": "067",
+      "cardID": "OP01-067",
+      "artwork": {
+        "half": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fonepiece.fandom.com%2Fwiki%2FCrocodile&psig=AOvVaw3AIn-HYZbajy8TD_b9UUsD&ust=1674240672202000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCMiOg9em1PwCFQAAAAAdAAAAABAQ",
+        "full": "https://static.wikia.nocookie.net/villains/images/0/08/Crocodile_OP_render.png/revision/latest?cb=20210815013824"
+      },
+      "health": "N/A",
+      "power": 7000,
+      "cost": 7,
+      "counter": 1000,
+      "text": "[Banish] (When this card deals damage, the target card is trashed without activating its Trigger.)\n[DON!! x1] Give blue Events in your hand -1 cost.",
+      "textAsHTML": "<span class=\"orange jagged\">Banish </span> <i>(When this card deals damage, the target card is trashed without activating its Trigger.)</i>\n<span class=\"black round\">DON!! x1</span> Give blue Events in your hand -1 cost."
+    },
+    "0002": {
+      "key": "0002",
+      "displayName": "Crocodile",
+      "type": "Character",
+      "colors": [
+        "blue"
+      ],
+      "damageType": "Special",
+      "subtypes": [
+        "The Seven Warlords of the Sea",
+        "Baroque Works"
+      ],
+      "image": {
+        "regular": "https://product-images.tcgplayer.com/fit-in/656x656/288268.jpg",
+        "special": ""
+      },
+      "box": "ST03",
+      "count": 2,
+      "boxPosition": "003",
+      "cardID": "ST03-003",
+      "artwork": {
+        "half": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fonepiece.fandom.com%2Fwiki%2FCrocodile&psig=AOvVaw3AIn-HYZbajy8TD_b9UUsD&ust=1674240672202000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCMiOg9em1PwCFQAAAAAdAAAAABAQ",
+        "full": "https://static.wikia.nocookie.net/villains/images/0/08/Crocodile_OP_render.png/revision/latest?cb=20210815013824"
+      },
+      "health": "N/A",
+      "power": 6000,
+      "cost": 5,
+      "counter": "N/A",
+      "text": "[Blocker] (After your opponent declares an attack, you may rest this card to make it the new target of the attack.)\n[DON!! x1] [On Block] Place up to 1 Character with a cost of 2 or less at the bottom of the owner's deck.",
+      "textAsHTML": "<span class = \"orange jagged\">Blocker </span><i> (After your opponent declares an attack, you may rest this card to make it the new target of the attack.)</i>\n<span class = \"black round\"> DON!! x1 </span> <span class=\"blue curved\">On Block</span> Place up to 1 Character with a cost of 2 or less at the bottom of the owner's deck."
+    },
+    "0004": {
+      "key": "0004",
+      "displayName": "Ms. All Sunday",
+      "type": "Character",
+      "colors": [
+        "blue"
+      ],
+      "damageType": "Strike",
+      "subtypes": [
+        "Baroque Works"
+      ],
+      "image": {
+        "regular": "https://product-images.tcgplayer.com/fit-in/656x656/454610.jpg",
+        "special": ""
+      },
+      "box": "OP01",
+      "count": "N/A",
+      "boxPosition": "079",
+      "cardID": "OP01-079",
+      "artwork": {
+        "half": "https://i.pinimg.com/736x/a8/35/38/a83538ec525138f582a5ff2eb47fee91.jpg",
+        "full": "https://cdn.inprnt.com/thumbs/af/1d/af1d5d75b0b5296658c0e239fbd4ba59.jpg"
+      },
+      "health": "N/A",
+      "power": 1000,
+      "cost": 3,
+      "counter": 1000,
+      "text": "[Blocker] (After your opponent declares an attack, you may rest this card to make it the new target of the attack.)\n[On K.O.] If your Leader has the \"Baroque Works\" type, add up to 1 Event from your trash to your hand.",
+      "textAsHTML": "<span class = \"orange jagged\">Blocker </span><i> (After your opponent declares an attack, you may rest this card to make it the new target of the attack.)</i>\n<span class=\"blue curved\">On K.O.</span> If your Leader has the \"Baroque Works\" type, add up to 1 Event from your trash to your hand."
+    },
+    "0005": {
+      "key": "0005",
+      "displayName": "Donquixote Doflamingo",
+      "type": "Character",
+      "colors": [
+        "blue"
+      ],
+      "damageType": "Special",
+      "subtypes": [
+        "The Seven Warlords of the Sea",
+        "Donquixote Pirates"
+      ],
+      "image": {
+        "regular": "https://product-images.tcgplayer.com/288279.jpg",
+        "special": "https://product-images.tcgplayer.com/420317.jpg"
+      },
+      "box": "ST03",
+      "count": "N/A",
+      "boxPosition": "009",
+      "cardID": "ST03-009",
+      "artwork": {
+        "half": "https://static.wikia.nocookie.net/onepiece/images/8/82/Doflamingo_Burning_Will.png/revision/latest?cb=20200626022404",
+        "full": "https://static.wikia.nocookie.net/onepiece/images/7/7e/Donquixote_Doflamingo_Anime_Infobox.png/revision/latest?cb=20221101232716"
+      },
+      "health": "N/A",
+      "power": 7000,
+      "cost": 7,
+      "counter": "N/A",
+      "text": "[On Play] Return up to 1 Character with a cost of 7 or less to the owner's hand.",
+      "textAsHTML": "<span class=\"blue curved\">On Play</span> Return up to 1 Character with a cost of 7 or less to the owner's hand."
+    }
+  }
+}
