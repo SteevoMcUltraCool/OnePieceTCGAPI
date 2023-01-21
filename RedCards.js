082d2d89feec4f7a7ee7833105b401b90cc87997
SteevoMcUltraCool <113132373+SteevoMcUltraCool@users.noreply.github.com>
2023-01-21 11:20:21 -0500

    Create RedCards.js

---
+++ RedCards.js
@@ -0,0 +1,35 @@
+{
+  "ref": Ref(Collection("Cards"), "354307479772856407"),
+  "ts": 1674227295990000,
+  "data": {
+    "EXAMPLE": {
+      "key": "0000",
+      "displayName": "Crocodile",
+      "type": "Leader",
+      "colors": [
+        "blue"
+      ],
+      "subtypes": [
+        "The Seven Warlords of the Sea",
+        "Baroque Works"
+      ],
+      "damageType": "Special",
+      "image": {
+        "regular": "https://product-images.tcgplayer.com/fit-in/437x437/288266.jpg",
+        "special": ""
+      },
+      "box": "ST03",
+      "count": 1,
+      "boxPosition": "001",
+      "cardID": "ST03-001",
+      "artwork": {
+        "half": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fonepiece.fandom.com%2Fwiki%2FCrocodile&psig=AOvVaw3AIn-HYZbajy8TD_b9UUsD&ust=1674240672202000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCMiOg9em1PwCFQAAAAAdAAAAABAQ",
+        "full": "https://static.wikia.nocookie.net/villains/images/0/08/Crocodile_OP_render.png/revision/latest?cb=20210815013824"
+      },
+      "health": 5,
+      "power": 5000,
+      "cost": "N/A",
+      "counter": "N/A",
+      "text": "[Activate: Main] [Once Per Turn] DON!! -4 (You may return the specified number of DON!! cards from your field to your DON!! deck.): Return up to 1 Character with a cost of 5 or less to the owner's hand.",
+      "textAsHTML": "<span class=\"blue curved\">Activate: Main</span> <span class=\"red round\">Once Per Turn<span> <b>DON!! -4</b> <i>(You may return the specified number of DON!! cards from your field to your DON!! deck.)</i>: Return up to 1 Character with a cost of 5 or less to the owner's hand."
+}
