98e0df2e2340cdb317d3f52d9ea20116aae04e78
SteevoMcCool <96311500+SteevoMcCool@users.noreply.github.com>
2023-01-21 13:16:00 -0500

    Update cards.js

--- cards.js
+++ cards.js
@@ -95,7 +95,7 @@ let a = {
     damageType: "Special",
     subtypes: ["The Seven Warlords of the Sea", "Thriller Bark Pirates"],
     image: {
-      regular: "https://product-images.tcgplayer.com/422215.jpg",
+      regular: "https://product-images.tcgplayer.com/288270.jpg",
       special: ""
     },
     box: "ST03",
@@ -104,17 +104,129 @@ let a = {
     cardID: "ST03-004",
     artwork: {
       half:
-        "https://preview.redd.it/5s8mjv4ksj551.jpg?auto=webp&s=f2fdfc815afc3bd1c4bd740332242c5171ed9d73",
+        "https://static.wikia.nocookie.net/onepiece/images/1/19/Gecko_Moria_at_Age_26.png/revision/latest?cb=20210425154812",
       full:
-        "https://static.wikia.nocookie.net/onepiece/images/b/be/Edward_Weevil_Anime_Infobox.png/revision/latest?cb=20160807113654"
+        "https://static.wikia.nocookie.net/onepiece/images/b/be/Gecko_Moria_Anime_Infobox.png/revision/latest?cb=20181127062446"
     },
     health: "N/A",
     power: 5000,
+    cost: 4,
+    counter: "N/A",
+    text: `[On Play] Add up to 1 "The Seven Warlords of the Sea" or "Thriller Bark Pirates" type Character with a cost of 4 or less than [Gecko Moria] from your trash to your hand.`,
+    textAsHTML: `<span class="blue curved">On Play</span> Add up to 1 "The Seven Warlords of the Sea" or "Thriller Bark Pirates" type Character with a cost of 4 or less than [Gecko Moria] from your trash to your hand.`
+  }, 
+  "0003": {
+    key: "0003",
+    displayName: "Dracule Mihawk",
+    type: "Character",
+    colors: ["blue"],
+    damageType: "Slash",
+    subtypes: ["The Seven Warlords of the Sea"],
+    image: {
+      regular: "https://product-images.tcgplayer.com/288272.jpg",
+      special: ""
+    },
+    box: "ST03",
+    count: 2,
+    boxPosition: "005",
+    cardID: "ST03-005",
+    artwork: {
+      half:
+        "https://staticg.sportskeeda.com/editor/2022/08/dc989-16615352305018-1920.jpg",
+      full:
+        "https://static.wikia.nocookie.net/onepiece/images/b/bf/Dracule_Mihawk_Anime_Infobox.png/revision/latest?cb=20151222105910"
+    },
+    health: "N/A",
+    power: 5000,
+    cost: 4,
+    counter: 2000,
+    text: `[DON!! x1] [When Attacking] Draw 2 cards and trash 2 cards from your hand.`,
+    textAsHTML: `<span class="black round">DON!! x1</span> <span class = "blue curved">When Attacking</span> Draw 2 cards and trash 2 cards from your hand.`
+  }, 
+  "0004": {
+    key: "0004",
+    displayName: "Jinbe",
+    type: "Character",
+    colors: ["blue"],
+    damageType: "Strike",
+    subtypes: ["Fish Man","The Seven Warlords of the Sea","The Sun Pirates"],
+    image: {
+      regular: "https://product-images.tcgplayer.com/288273.jpg",
+      special: "https://product-images.tcgplayer.com/421875.jpg"
+    },
+    box: "ST03",
+    count: 4,
+    boxPosition: "006",
+    cardID: "ST03-006",
+    artwork: {
+      half:
+        "https://practicaltyping.com/wp-content/uploads/2018/09/jinbe.png",
+      full:
+        "https://64.media.tumblr.com/cc6a01a1c90cb1bc1424d29ddec2aca3/7a86b9e67d6d5ee2-51/s1280x1920/ffb6248554ff3d3312fdd5efefadb3ad98194e9f.jpg"
+    },
+    health: "N/A",
+    power: 4000,
+    cost: 2,
+    counter: 1000,
+    text: ``,
+    textAsHTML: ``
+  }, 
+  "0005": {
+    key: "0005",
+    displayName: "Sentomaru",
+    type: "Character",
+    colors: ["blue"],
+    damageType: "Slash",
+    subtypes: ["Navy"],
+    image: {
+      regular: "https://product-images.tcgplayer.com/288275.jpg",
+      special: ""
+    },
+    box: "ST03",
+    count: 2,
+    boxPosition: "007",
+    cardID: "ST03-007",
+    artwork: {
+      half:
+        "https://static.wikia.nocookie.net/onepiece/images/9/97/Sentomaru_Anime_Post_Timeskip_Infobox.png/revision/latest?cb=20111016165818",
+      full:
+        "https://static.wikia.nocookie.net/onepiece/images/9/97/Sentomaru_Anime_Post_Timeskip_Infobox.png/revision/latest?cb=20111016165818"
+    },
+    health: "N/A",
+    power: 4000,
     cost: 3,
     counter: 1000,
-    text: "",
-    textAsHTML: ""
-  }, 
+    text: `[DON!! x1] [Activate: Main] [Once Per Turn] (2) (You may rest the specified number of DON!! cards in your cost area.): Play up to 1 [Pacifista] with a cost of 4 or less from your deck, then shuffle your deck.`,
+    textAsHTML: `<span class ="black round">DON!! x1</span> <span class="blue curved">Activate: Main</span> <span class="red round">Once Per Turn</span> <span class= "gray round"> 2</span> <i>(You may rest the specified number of DON!! cards in your cost area.)</i>: Play up to 1 [Pacifista] with a cost of 4 or less from your deck, then shuffle your deck.`
+  },
+  "0006": {
+    key: "0006",
+    displayName: "Trafalgar Law",
+    type: "Character",
+    colors: ["blue"],
+    damageType: "Slash",
+    subtypes: ["The Seven Warlords of the Sea", "Heart Pirates"],
+    image: {
+      regular: "https://product-images.tcgplayer.com/288277.jpg",
+      special: ""
+    },
+    box: "ST03",
+    count: 2,
+    boxPosition: "008",
+    cardID: "ST03-008",
+    artwork: {
+      half:
+        "https://imgix.ranker.com/list_img_v2/5657/3065657/original/3065657",
+      full:
+        "https://preview.redd.it/mp578sis3pq71.jpg?auto=webp&s=e48b952ec68569d982fd38d65d57cc14d4f115e3"
+    },
+    health: "N/A",
+    power: 4000,
+    cost: 3,
+    counter: 1000,
+    text: `[Blocker] (After your opponent declares an attack, you may rest this card to make it the new target of the attack.)`,
+    textAsHTML: `<span class="orange jagged">Blocker</span> <i>(After your opponent declares an attack, you may rest this card to make it the new target of the attack.)</i>`
+  },
   "0007": {
     key: "0007",
     displayName: "Donquixote Doflamingo",
